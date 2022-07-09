9ee82089b443e237b2bb03bc5bfe66a5df84bdad
Igor <membship@ya.ru>
2022-07-09 19:37:09 +0700

    first commit

--- result/js/common.js
+++ result/js/common.js
@@ -34,6 +34,61 @@ if ( $('.eqslider__slider').length ) {
 }
 
 
+// CATALOG SIMILARY SLIDER
+$(InitCatalogSimilarySlider);
+$(document).on("reload", InitCatalogSimilarySlider);
+function InitCatalogSimilarySlider() {
+  var $sliders = $('.catalog-similary-slider:not(.js-swiper-initialized)');
+  if ($sliders.length > 0) {
+    $sliders.each(function () {
+      var $swiperBlock = $(this);
+      
+      var swiperOpts = {
+        init: true,
+        loop: false,
+        speed: 300,
+        navigation: {
+          prevEl: $swiperBlock.find('.swiper-button.prev'),
+          nextEl: $swiperBlock.find('.swiper-button.next')
+        },
+        initialSlide: 0,
+        slidesPerView: 4,
+        slidesPerGroup: 1,
+        spaceBetween: 30,
+        pagination: {
+          el: $swiperBlock.find('.swiper-pagination'),
+          clickable: true,
+          type: 'bullets'
+        },
+        breakpoints: {
+          1799: {
+            slidesPerView: 5
+          },
+          1399: {
+            slidesPerView: 4
+          },
+          1199: {
+            slidesPerView: 4,
+            spaceBetween: 16,
+          },
+          1023: {
+            slidesPerView: 3,
+            spaceBetween: 16,
+          },
+          767: {
+            slidesPerView: 'auto',
+            spaceBetween: 10,
+          }
+        }
+      };
+      
+      var sw = new Swiper($swiperBlock.find('.swiper-container'), swiperOpts);
+      $swiperBlock.data('swiper', sw).addClass('js-swiper-initialized');
+    });
+  }
+}
+
+
 }); //ready
 
 
